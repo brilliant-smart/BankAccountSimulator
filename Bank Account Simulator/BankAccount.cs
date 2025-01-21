921979ad3ffeb7809a67863974138b25cfaea9a6
Brilliant Smart <53043496+brilliant-smart@users.noreply.github.com>
2025-01-21 15:31:17 +0100

    Add project files.

---
+++ Bank Account Simulator/BankAccount.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Account_Simulator
+{
+    internal class BankAccount
+    {
+        public int AccountNumber { get; set; }
+        public string AccountName { get; set; }
+        public decimal AccountBalance { get; set; }
+        private List<string> _transactionHistory;
+
+
+        //constructor
+        public BankAccount(int accountNumber, string accountName, decimal initialBalance = 0)
+        {
+            AccountNumber = accountNumber;
+            AccountName = accountName;
+            AccountBalance = initialBalance;
+            _transactionHistory = new List<string>();
+        }
+
+        //methods
+
+        public void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                AccountBalance += amount;
+
+                _transactionHistory.Add($"₦{amount} Deposited Successfully");
+
+                Console.WriteLine($"₦{amount} successfully deposited");
+            }
+            else
+            {
+                Console.WriteLine("Deposite amount must be positive");
+            }
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= AccountBalance)
+            {
+                AccountBalance -= amount;
+
+                _transactionHistory.Add($"₦{amount} Withdrawn successfully");
+
+                Console.WriteLine($"₦{amount} is withdrawn successfully");
+            }
+            else if(amount > AccountBalance)
+            {
+                Console.WriteLine("Insufficient Fund");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid amount that is greater than zero");
+            }
+        }
+
+        public decimal GetBalance()
+        {
+            return AccountBalance;
+
+        }
+
+        public void GetTransactionHistory()
+        {
+            foreach (var transaction in _transactionHistory)
+            {
+                Console.Write(transaction);
+            }
+        }
+
+    }
+}
---
+++ Bank Account Simulator/Program.cs
@@ -0,0 +1,72 @@
+﻿using Bank_Account_Simulator;
+
+namespace Bank_Account_Simulator
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Welcome to the bank simulation app");
+
+            Console.WriteLine("Please enter your full name to get started");
+            string accountName = Console.ReadLine();
+
+            Random rand = new Random();
+            int accountNumber = rand.Next(1000, 9999);
+
+            Console.WriteLine($"Welcome {accountName}, your account number is: {accountNumber}");
+
+            BankAccount bankAccount = new BankAccount(accountNumber, accountName,0);
+
+
+            while (true)
+            {
+                Console.WriteLine($"your current account balance is {bankAccount.GetBalance()}");
+                Console.WriteLine($"Press 1 to get your account balance");
+                Console.WriteLine($"Press 2 to deposit");
+                Console.WriteLine($"Press 3 to withdraw");
+                Console.WriteLine($"Press 4 to get your transactions history");
+                Console.WriteLine($"Press 5 to Exit");
+
+
+
+                int transactionChoice = int.Parse(Console.ReadLine());
+
+
+                switch (transactionChoice)
+                {
+                    case 1:
+                        Console.WriteLine($"Your current account balance is: {bankAccount.GetBalance()}");
+                        break;
+
+                    case 2:
+                        Console.WriteLine($"Please enter the amount you would like to deposit here:");
+                        decimal depositAmount = decimal.Parse(Console.ReadLine());
+                        bankAccount.Deposit(depositAmount);
+                        break;
+
+                    case 3:
+                        Console.WriteLine($"Please enter the amount you would like to withdraw");
+                        decimal withdrawAmount = decimal.Parse(Console.ReadLine());
+                        bankAccount.Withdraw(withdrawAmount);
+                        break;
+                    case 4:
+                        Console.WriteLine($"Your transactions history is: ");
+                        bankAccount.GetTransactionHistory();
+                        break;
+                    case 5:
+                        Console.WriteLine("Thank you for banking with us");
+                        break;
+                    default:
+                        Console.WriteLine("wrong choice please try again");
+                        break;
+                }
+            }
+                
+            
+        }
+    }
+}
